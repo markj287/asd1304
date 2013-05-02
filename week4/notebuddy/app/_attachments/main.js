79a325590f78aeac65406af80f1c245fd0b7e130
markj287 <developer.johnson.mark@gmail.com>
2013-05-02 19:43:52 -0400

    Added a few function in main.js

--- week4/notebuddy/app/_attachments/main.js
+++ week4/notebuddy/app/_attachments/main.js
@@ -2,3 +2,68 @@
 // ASD1304 
 // Notebuddy
 // Project 4 CouchDB App
+
+$('#createNote').on('pageinit', function (){
+	var noteForm = $('#addItem');
+	noteForm.validate({
+		invalidHandler: function (form, validator) {
+
+		},
+		submitHandler: function () {
+			var data = noteForm.serializeArray();
+			if ($('#submit').val() === "Save Note") {
+				storeData();
+			}
+		}
+});
+	
+var title = $('#title');
+	selectCat = $('#selectCat');
+	date = $('#dadded');
+	where = $('#where');
+	note = $('#notes');
+	
+	function storeData() {
+		var favorite = $(':radio:checked').val();
+		var display = getCheckboxValue();
+		var item			= {};
+			item._id		= ("note" + $.now());
+			item.title		= title.val();
+			item.selectCat	= selectCat.val();
+			item.dadded		= dadded.val();
+			item.where		= where.val();
+			item.favorite	= favorite;
+			item.display	= display;
+			item.amount		= amount.val();
+			itme.notes		= notes.val();
+			
+		$.couch.db("notebuddy").saveDoc(item, {
+			success: function(data) {
+				console.log(data);
+			}
+		});
+		alert("Note Saved!");
+		$.mobile.changePage('#viewNotes');
+		window.location.reload();
+	}
+	
+	function getCheckboxValue() {
+		var checkboxes = $(':checkbox:checked');
+		var holdValues = [];
+		for (var i=0, j=checkboxes.length; i<j; i++){
+			if(checkboxes[i].checked){
+				var checkedValue = checkboxes[i].value;
+				holdValues.push(checkedValue);
+			}
+		}
+		return holdValues;	
+	}
+});
+
+
+
+
+
+
+
+
