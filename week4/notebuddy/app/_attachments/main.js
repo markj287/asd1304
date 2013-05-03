eb5e5b428350bdadd09c51e9bc5794bc9a0eef28
markj287 <developer.johnson.mark@gmail.com>
2013-05-02 20:53:00 -0400

    finished up main.js

--- week4/notebuddy/app/_attachments/main.js
+++ week4/notebuddy/app/_attachments/main.js
@@ -33,7 +33,6 @@ var title = $('#title');
 			item.dadded		= dadded.val();
 			item.where		= where.val();
 			item.favorite	= favorite;
-			item.display	= display;
 			item.amount		= amount.val();
 			itme.notes		= notes.val();
 			
@@ -58,6 +57,144 @@ var title = $('#title');
 		}
 		return holdValues;	
 	}
+});
+
+$('#viewNotes').on("pageinit", function() {
+	$.couch.db("notebuddy").view("notebuddy/notes", {
+		success: function(data) {
+			$("#noteList").empty();
+			$.each(data.rows, function(index, note) {
+				var key = note.key
+					title = note.value.title
+					dadded = note.value.dadded
+					where = note.value.where
+					favorite = note.value.favorite
+					amount = note.value.amount
+					notes = note.value.notes
+					id = note.id
+					rev = note.value.rev
+				$(''+
+					'<div id="'+ id +'" data-role="collapsible" data-content-theme="a">'+
+						'<h4>'+ key +'</h4>'+
+						'<p>Title: '+ title +'</p>'+
+						'<p>Date: '+ dadded +'</p>'+
+						'<p>Where: '+ where +'</p>'+
+						'<p>Favorite? '+ favorite +'</p>'+
+						'<p>Amount: '+ amount +'</p>'+
+						'<p>Notes '+ notes +'</p>'+
+						'<li><a class="editLink" data-role="button" data-inline="true" data-theme="b" href="#createNote" data-mini="true">Edit Note</a>'+
+						'<a class="deleteLink" data-role="button" data-inline="true" data-theme="b" href="#" data-mini="true">Delete Note</a>'+
+						'</li>'+
+					'</div>'
+				).appendTo('#noteList');
+				makeItemLinks(id, rev);
+			});
+
+		$('#noteList').trigger("create");
+		
+		}
+});
+	
+	function makeItemLinks(id, rev) {
+		var deleteLink = $('#'+ id +' .deleteLink');
+		deleteLink.on("click", function() {
+			var ask = confirm("Delete This Note.");
+			if (ask) {
+				deleteNote(id, rev);
+				alert("Note was deleted!");
+				window.location.reload();
+			} else {
+				alert("Note was not deleted!");
+			}
+		});
+		
+		var editLink = $('#'+ id +' .editLink');
+		editLink.on("click", function() {
+			$.couch.db("notebuddy").openDoc(id, {
+			    success: function(note) {
+				    $.mobile.changePage('#createNote');
+					
+					//populate form fields with current values
+					$('#title').val(''+ note.title);
+					$("#selectCat option[value=" + note.selectCat +"]").attr("selected","selected");
+					$('#selectCat').selectmenu('refresh', true);
+					$('#dadded').val(''+ note.dadded);
+					$('#where').val(''+ note.where);
+					$("input[type='radio'][value="+ note.favorite +"]").attr("checked", "true").checkboxradio("refresh");
+					$('#amount').val(''+ note.amount);
+					$('#notes').val(''+ note.notes);
+										
+					//Change submit button value to edit button
+					$('#headerBar').html('Edit Note');
+					$('#submit').val('Edit Note');
+					var editSubmit = $('#submit');
+					
+					editSubmit.on("click", function(){
+						var title = $('#title');
+							selectCat = $('#selectCat');
+							date = $('#dadded');
+							where = $('#where');
+							note = $('#notes');
+						var favorite = $(':radio:checked').val();
+						var favorite = getCheckboxValue();
+						
+						var note			= {};
+							note._id		= id;
+							note._rev		= rev;
+							note.title		= title.val();
+							note.date		= date.val();
+							note.where		= where.val();
+							note.favorite	= favorite.val();
+							note.selectCat	= selectCat;
+							note.amount		= amount.val();
+							note.notes		= notes.val();
+							
+							//log out the note to see if its working 
+							console.log(note);
+						
+						$.couch.db("notebuddy").saveDoc(note, {
+						    success: function(data) {
+						    	// log out data to see if its working 
+						        console.log(data);
+						    }
+						});	
+						
+						alert("Note Was Updated!");
+						$.mobile.changePage('#viewNotes');
+						window.location.reload();
+							
+						function getCheckboxValue() {
+							var checkboxes = $(':checkbox:checked');
+							var holdValues = [];
+							for (var i=0, j=checkboxes.length; i<j; i++){
+								if(checkboxes[i].checked){
+									var checkedValue = checkboxes[i].value;
+									holdValues.push(checkedValue);
+								}
+							}
+							return holdValues;	
+						}
+					
+					});
+				}
+			});
+		});
+	}
+	
+	function deleteNote(id, rev) {
+		var doc = {
+			_id: ""+ id +"",
+			_rev: ""+ rev +""
+		};
+		$.couch.db("notebuddy").removeDoc(doc, {
+			success: function(data) {
+				// log out data to see if its working 
+				console.log(data);
+				$('#viewNotes').trigger("create");
+			}
+		});
+	}
+	
 });
 
 
